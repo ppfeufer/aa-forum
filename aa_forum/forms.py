f2b80e755cac6b4ebcf632236b3f9d5af92185c6
Peter Pfeufer <info@ppfeufer.de>
2021-06-04 14:57:45 +0200

    new topic form

---
+++ aa_forum/forms.py
@@ -0,0 +1,60 @@
+"""
+Forms
+"""
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+from django import forms
+from django.forms import ModelForm
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
+
+from aa_forum.models import Messages
+
+
+def get_mandatory_form_label_text(text):
+    """
+    Label text for mandatory form fields
+    :param text:
+    :type text:
+    :return:
+    :rtype:
+    """
+
+    required_text = _("This field is mandatory")
+    required_marker = (
+        f'<span aria-label="{required_text}" class="form-required-marker">*</span>'
+    )
+
+    return mark_safe(
+        f'<span class="form-field-required">{text} {required_marker}</span>'
+    )
+
+
+class NewTopicForm(ModelForm):
+    """
+    Fat link form
+    used to create ESI fatlinks
+    """
+
+    subject = forms.CharField(
+        required=True,
+        label=get_mandatory_form_label_text(_("Subject")),
+        max_length=255,
+        widget=forms.TextInput(attrs={"placeholder": _("Subject")}),
+    )
+
+    message = forms.CharField(
+        widget=CKEditorUploadingWidget(
+            attrs={"rows": 10, "cols": 20, "style": "width: 100%;"}
+        ),
+        required=True,
+        label=get_mandatory_form_label_text(_("Message")),
+    )
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """
+        meta definitions
+        """
+
+        model = Messages
+        fields = ["subject", "message"]
