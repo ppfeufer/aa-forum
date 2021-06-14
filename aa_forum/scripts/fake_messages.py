30de23bcfdd874a59171ab26373407ef3f93ab33
Peter Pfeufer <info@ppfeufer.de>
2021-06-14 12:49:08 +0200

    updated fake_message script

--- aa_forum/scripts/fake_messages.py
+++ aa_forum/scripts/fake_messages.py
@@ -6,25 +6,48 @@ from faker import Faker
 
 from django.utils import timezone
 
-from aa_forum.models import Messages, Topics
+from aa_forum.models import Boards, Messages, Topics
 
 
 def run():
     """
-    Create some random messages for topic id 1
+    Create some random topics and messages
     """
 
     fake = Faker()
 
-    topics = Topics.objects.all()
-    time_posted = timezone.now()
+    # Add some topics
+    boards = Boards.objects.all()
+    if boards.count() > 0:
+        for board in boards:
+            time_posted = timezone.now()
 
+            for _ in range(25):
+                topic = Topics()
+                topic.board = board
+                topic.user_started_id = 2
+                topic.user_updated_id = 2
+                topic.time_modified = time_posted
+                topic.subject = fake.sentence()
+                topic.save()
+
+                message = Messages()
+                message.topic = topic
+                message.board = board
+                message.time_posted = time_posted
+                message.time_modified = time_posted
+                message.user_created_id = 2
+                message.message = f"<p>{fake.sentence()}</p>"
+                message.save()
+
+    # Add some messages to topics
+    topics = Topics.objects.all()
     if topics.count() > 0:
         for topic in topics:
             Messages.objects.bulk_create(
                 [
                     Messages(
-                        message=fake.sentence(),
+                        message=f"<p>{fake.sentence()}</p>",
                         time_posted=time_posted,
                         time_modified=time_posted,
                         board_id=topic.board_id,
