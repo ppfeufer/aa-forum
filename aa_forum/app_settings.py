ad9bdde02c04e12a8e752cadaf841fc53a5d7cc7
ErikKalkoken <erik.kalkoken@gmail.com>
2021-06-20 17:34:06 +0200

    Remove old code fro app_settings.py

--- aa_forum/app_settings.py
+++ aa_forum/app_settings.py
@@ -1,17 +1,3 @@
 """
 Our app setting
 """
-
-from aasrp.utils import clean_setting
-
-# AA-GDPR
-AVOID_CDN = clean_setting("AVOID_CDN", False)
-
-
-def avoid_cdn() -> bool:
-    """
-    Check if we should avoid CDN usage
-    :return: bool
-    """
-
-    return AVOID_CDN
