f172d301c31eb86902b83b51de5369890bfe2099
Peter Pfeufer <info@ppfeufer.de>
2021-06-28 01:51:48 +0200

    [FIX] board order on mass creation

--- aa_forum/views/admin.py
+++ aa_forum/views/admin.py
@@ -103,6 +103,7 @@ def category_create(request: WSGIRequest) -> HttpResponseRedirect:
 
                 for board_name in boards.splitlines():
                     new_board = Board(name=board_name, category=new_category)
+                    new_board.order = 999999
                     new_board.save()
 
             messages.success(
