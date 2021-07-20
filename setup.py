5b6db421933fdd077930202ea1772178dffa9263
Peter Pfeufer <info@ppfeufer.de>
2021-07-20 19:54:09 +0200

    setup.py improved

--- setup.py
+++ setup.py
@@ -16,39 +16,58 @@ with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+package_name = "aa-forum"
+package_description = "Simple forum for Alliance Auth"
+package_license = "GPLv3"
+package_author = "Peter Pfeufer"
+package_author_email = "development@ppfeufer.de"
+package_git_url = "https://github.com/ppfeufer/aa-forum"
+package_issues_url = "https://github.com/ppfeufer/aa-forum/issues"
+package_changelog_url = "https://github.com/ppfeufer/aa-forum/blob/master/CHANGELOG.md"
+package_install_requirements = [
+    "allianceauth>=2.8.2",
+    "django-ckeditor",
+    "allianceauth-app-utils>=1.8.0",
+]
+package_classifiers = [
+    "Environment :: Web Environment",
+    "Framework :: Django",
+    "Framework :: Django :: 3.1",
+    "Framework :: Django :: 3.2",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+]
+
+# URLs are listed in reverse on Pypi
+project_urls = {
+    "Issue / Bug Reports": package_issues_url,
+    "Change Log": package_changelog_url,
+    "Git Repository": package_git_url,
+}
+
 setup(
-    name="aa-forum",
+    name=package_name,
     version=__version__,
     packages=find_packages(),
     include_package_data=True,
-    license="GPLv3",
-    description="Simple forum for Alliance Auth",
+    license=package_license,
+    description=package_description,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ppfeufer/aa-forum",
-    author="Peter Pfeufer",
-    author_email="development@ppfeufer.de",
-    classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Framework :: Django :: 3.1",
-        "Framework :: Django :: 3.2",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-    ],
+    url=package_git_url,
+    project_urls=project_urls,
+    author=package_author,
+    author_email=package_author_email,
+    classifiers=package_classifiers,
     python_requires="~=3.6",
-    install_requires=[
-        "allianceauth>=2.8.2",
-        "django-ckeditor",
-        "allianceauth-app-utils>=1.8.0",
-    ],
+    install_requires=package_install_requirements,
 )
