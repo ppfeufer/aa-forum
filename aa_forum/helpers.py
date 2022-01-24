08104d044fa533a6ffd688fcd2d9ee09eebbb8d9
Peter Pfeufer <info@ppfeufer.de>
2022-01-24 08:36:07 +0100

    Helper function `send_message_to_discord_webhook` added

--- aa_forum/helpers.py
+++ aa_forum/helpers.py
@@ -5,11 +5,25 @@ Our little helpers
 # Standard Library
 import re
 
+# Third Party
+import requests
+
 # Django
 from django.contrib import messages
 from django.core.handlers.wsgi import WSGIRequest
 from django.forms import Form
 from django.utils.translation import gettext_lazy
+
+# Alliance Auth
+from allianceauth.services.hooks import get_extension_logger
+
+# Alliance Auth (External Libs)
+from app_utils.logging import LoggerAddTag
+
+# AA Forum
+from aa_forum import __title__
+
+logger = LoggerAddTag(get_extension_logger(__name__), __title__)
 
 
 def message_form_errors(request: WSGIRequest, form: Form) -> None:
@@ -39,3 +53,22 @@ def string_cleanup(string: str) -> str:
     string = re_css.sub("", string)
 
     return string
+
+
+def send_message_to_discord_webhook(webhook_url: str, text_message: str, embeds: {}):
+    """
+    Send a message to a Discord Webhook
+    :param webhook_url:
+    :param text_message:
+    :param embeds:
+    :return:
+    """
+
+    data = {"content": text_message, "embeds": [embeds]}
+
+    result = requests.post(webhook_url, json=data)
+
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        logger.info(f"Discord Webhook Error: {err}")
