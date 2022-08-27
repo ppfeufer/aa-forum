767741f53cd480a43cfa64f2c2f193bb50bcf445
Peter Pfeufer <info@ppfeufer.de>
2022-08-27 19:37:57 +0200

    [ADDED] Logger to messages view

--- aa_forum/views/messages.py
+++ aa_forum/views/messages.py
@@ -7,12 +7,25 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# Alliance Auth
+from allianceauth.services.hooks import get_extension_logger
+
+# Alliance Auth (External Libs)
+from app_utils.logging import LoggerAddTag
+
+# AA Forum
+from aa_forum import __title__
+
+logger = LoggerAddTag(get_extension_logger(__name__), __title__)
+
 
 def index(request: WSGIRequest) -> HttpResponse:
     """
     Messages overview
     :return:
     """
+
+    logger.info(f"{request.user} called their messages overview")
 
     context = {}
 
