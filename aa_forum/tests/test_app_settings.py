4c169cc5dbd5502f0b461bee8913e68f010cb5f7
Peter Pfeufer <info@ppfeufer.de>
2022-09-14 20:34:58 +0200

    [ADDED] Checks for installed modules

---
+++ aa_forum/tests/test_app_settings.py
@@ -0,0 +1,45 @@
+"""
+Testing app settings
+"""
+
+# Django
+from django.test import TestCase, modify_settings
+
+# AA Forum
+from aa_forum.app_settings import (
+    allianceauth_discordbot_installed,
+    discord_messaging_proxy_available,
+)
+
+
+class TestModulesInstalled(TestCase):
+    """
+    Test for installed modules
+    """
+
+    @modify_settings(INSTALLED_APPS={"append": "aadiscordbot"})
+    def test_allianceauth_discordbot_installed_should_return_true(self):
+        """
+        Test allianceauth_discordbot_installed should return True
+        :return:
+        """
+
+        self.assertTrue(allianceauth_discordbot_installed())
+
+    @modify_settings(INSTALLED_APPS={"remove": "aadiscordbot"})
+    def test_allianceauth_discordbot_installed_should_return_false(self):
+        """
+        Test allianceauth_discordbot_installed should return False
+        :return:
+        """
+
+        self.assertFalse(allianceauth_discordbot_installed())
+
+    @modify_settings(INSTALLED_APPS={"append": "aadiscordbot"})
+    def test_discord_messaging_proxy_available_return_true(self):
+        """
+        Test discord_messaging_proxy_available should return True for aadiscordbot
+        :return:
+        """
+
+        self.assertTrue(discord_messaging_proxy_available())
