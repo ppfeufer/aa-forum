406e3c1768848519037721b575eb93527101ade5
Peter Pfeufer <info@ppfeufer.de>
2023-12-26 17:22:01 +0100

    [MISC] Test suite updated

--- aa_forum/tests/test_auth_hooks.py
+++ aa_forum/tests/test_auth_hooks.py
@@ -1,79 +1,79 @@
-# """
-# Test auth_hooks
-# """
-#
-# # Standard Library
-# from http import HTTPStatus
-#
-# # Django
-# from django.test import TestCase
-# from django.urls import reverse
-#
-# # AA Forum
-# from aa_forum.tests.utils import create_fake_user
-#
-#
-# class TestHooks(TestCase):
-#     """
-#     Test the app hook into allianceauth
-#     """
-#
-#     @classmethod
-#     def setUpClass(cls) -> None:
-#         """
-#         Set up groups and users
-#         """
-#
-#         super().setUpClass()
-#
-#         # User cannot access
-#         cls.user_1001 = create_fake_user(
-#             character_id=1001, character_name="Peter Parker"
-#         )
-#
-#         # User can access
-#         cls.user_1002 = create_fake_user(
-#             character_id=1002,
-#             character_name="Bruce Wayne",
-#             permissions=["aa_forum.basic_access"],
-#         )
-#
-#         cls.html_menu = f"""
-#             <li class="d-flex flex-wrap m-2 p-2 pt-0 pb-0 mt-0 mb-0 me-0 pe-0">
-#                 <i class="nav-link fas fa-comments fa-fw align-self-center me-3 "></i>
-#                 <a class="nav-link flex-fill align-self-center" href="{reverse('aa_forum:forum_index')}">
-#                     Forum
-#                 </a>
-#             </li>
-#         """
-#
-#     def test_render_hook_success(self):
-#         """
-#         Test should show the link to the app in the navigation to user with access
-#
-#         :return:
-#         :rtype:
-#         """
-#
-#         self.client.force_login(user=self.user_1002)
-#
-#         response = self.client.get(path=reverse(viewname="authentication:dashboard"))
-#
-#         self.assertEqual(first=response.status_code, second=HTTPStatus.OK)
-#         self.assertContains(response=response, text=self.html_menu, html=True)
-#
-#     def test_render_hook_fail(self):
-#         """
-#         Test should not show the link to the app in the
-#         navigation to user without access
-#
-#         :return:
-#         :rtype:
-#         """
-#
-#         self.client.force_login(user=self.user_1001)
-#
-#         response = self.client.get(path=reverse(viewname="authentication:dashboard"))
-#
-#         self.assertEqual(first=response.status_code, second=HTTPStatus.OK)
-#         self.assertNotContains(response=response, text=self.html_menu, html=True)
+"""
+Test auth_hooks
+"""
+
+# Standard Library
+from http import HTTPStatus
+
+# Django
+from django.test import TestCase
+from django.urls import reverse
+
+# AA Forum
+from aa_forum.tests.utils import create_fake_user
+
+
+class TestHooks(TestCase):
+    """
+    Test the app hook into allianceauth
+    """
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """
+        Set up groups and users
+        """
+
+        super().setUpClass()
+
+        # User cannot access
+        cls.user_1001 = create_fake_user(
+            character_id=1001, character_name="Peter Parker"
+        )
+
+        # User can access
+        cls.user_1002 = create_fake_user(
+            character_id=1002,
+            character_name="Bruce Wayne",
+            permissions=["aa_forum.basic_access"],
+        )
+
+        cls.html_menu = f"""
+            <li class="d-flex flex-wrap m-2 p-2 pt-0 pb-0 mt-0 mb-0 me-0 pe-0">
+                <i class="nav-link fas fa-comments fa-fw align-self-center me-3 "></i>
+                <a class="nav-link flex-fill align-self-center" href="{reverse('aa_forum:forum_index')}">
+                    Forum
+                </a>
+            </li>
+        """
+
+    def test_render_hook_success(self):
+        """
+        Test should show the link to the app in the navigation to user with access
+
+        :return:
+        :rtype:
+        """
+
+        self.client.force_login(user=self.user_1002)
+
+        response = self.client.get(path=reverse(viewname="authentication:dashboard"))
+
+        self.assertEqual(first=response.status_code, second=HTTPStatus.OK)
+        self.assertContains(response=response, text=self.html_menu, html=True)
+
+    def test_render_hook_fail(self):
+        """
+        Test should not show the link to the app in the
+        navigation to user without access
+
+        :return:
+        :rtype:
+        """
+
+        self.client.force_login(user=self.user_1001)
+
+        response = self.client.get(path=reverse(viewname="authentication:dashboard"))
+
+        self.assertEqual(first=response.status_code, second=HTTPStatus.OK)
+        self.assertNotContains(response=response, text=self.html_menu, html=True)
--- testauth/settings/base.py
+++ testauth/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django_bootstrap5",  # https://github.com/zostera/django-bootstrap5
     "sortedm2m",
     "esi",
+    "allianceauth.framework",
     "allianceauth.authentication",
     "allianceauth.services",
     "allianceauth.eveonline",
@@ -76,6 +77,7 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 MIDDLEWARE = [
+    "allianceauth.menu.middleware.MenuSyncMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "allianceauth.authentication.middleware.UserSettingsMiddleware",
@@ -129,6 +131,7 @@ WSGI_APPLICATION = "allianceauth.wsgi.application"
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -151,10 +154,15 @@ AUTHENTICATION_BACKENDS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
+
 LANGUAGE_CODE = "en-us"
+
 LANGUAGE_COOKIE_AGE = 1209600
+
 TIME_ZONE = "UTC"
+
 USE_I18N = True
+
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
@@ -201,10 +209,10 @@ LOGOUT_REDIRECT_URL = "authentication:dashboard"  # destination after logging ou
 # - relative urls eg '/dashboard'
 # - absolute urls eg 'http://example.com/dashboard'
 
-# Scopes required on new tokens when logging in. Cannot be blank.
+# scopes required on new tokens when logging in. Cannot be blank.
 LOGIN_TOKEN_SCOPES = ["publicData"]
 
-# Number of days email verification links are valid for
+# number of days email verification links are valid for
 ACCOUNT_ACTIVATION_DAYS = 1
 
 ESI_API_URL = "https://esi.evetech.net/"
@@ -226,7 +234,7 @@ LOGGING = {
             "filename": os.path.join(BASE_DIR, "log/allianceauth.log"),
             "formatter": "verbose",
             "maxBytes": 1024 * 1024 * 5,  # edit this line to change max log file size
-            "backupCount": 5,  # edit this line to change the number of log backups
+            "backupCount": 5,  # edit this line to change number of log backups
         },
         "extension_file": {
             "level": "INFO",
